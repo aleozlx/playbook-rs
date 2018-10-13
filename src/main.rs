67c4a6897068076c4529926bfbec0d0ae86b4888
Alex Yang <aleozlx@gmail.com>
2018-10-13 05:15:28 -0500

    + run_yaml()

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,27 @@
 #[macro_use] extern crate log;
 extern crate fern;
 extern crate chrono;
+extern crate yaml_rust;
+
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::result::Result;
+use std::collections::BTreeMap;
+use yaml_rust::YamlLoader;
 
 fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
-        .format(|out, message, _record| {
+        .format(|out, message, record| {
             out.finish(format_args!(
-                "{} {}",
+                "{} {} {}",
                 chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+                record.level(),
                 message
             ))
         })
         .level(log::LevelFilter::Debug)
         .chain(std::io::stderr())
-        // .chain(fern::log_file("output.log")?)
         .apply()?;
     Ok(())
 }
@@ -26,6 +34,15 @@ fn inside_docker() -> bool {
         Some(code) => code==0,
         None => unreachable!()
     }
+}
+
+fn run_yaml<P: AsRef<Path>>(playbook: P) -> Result<(), std::io::Error> {
+    // TODO propagate relocated path into ctx
+    let mut file = File::open(playbook)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let config = YamlLoader::load_from_str(&contents);
+    Ok(())
 }
 
 fn main() {
@@ -42,15 +59,18 @@ fn main() {
         panic!("ERROR {}", e);
     }
 
-    let playbook = std::path::Path::new(matches.value_of("PLAYBOOK").unwrap());
-    let playbook = if inside_docker() && playbook.is_absolute() {
-            // Absolute path to the playbook must be self-mounted with relocation specified at cmdline,
-            //   because we cannot read any content of the playbook without locating it first.
-            std::path::Path::new(matches.value_of("RELOCATE").expect("Missing a `--relocate` flag"))
-                .join(std::path::Path::new(matches.value_of("PLAYBOOK").unwrap()).file_name().unwrap())
+    let playbook = Path::new(matches.value_of("PLAYBOOK").unwrap());
+    let ret = if inside_docker() && playbook.is_absolute() {
+        // Absolute path to the playbook must be self-mounted with relocation specified at cmdline,
+        //   because we cannot read any content of the playbook without locating it first.
+        run_yaml(Path::new(matches.value_of("RELOCATE").expect("Missing a `--relocate` flag"))
+            .join(playbook.file_name().unwrap()))
     }
     else {
-        playbook.to_path_buf()
+        run_yaml(playbook)
     };
-
+    if let Err(e) = ret {
+        error!("{}: {}", e, playbook.display());
+        std::process::exit(2);
+    }
 }
