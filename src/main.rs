3ce7d9f0f4d2490cc64a90c4a2dbb7791e128953
Alex Yang <aleozlx@gmail.com>
2018-10-13 04:43:31 -0500

    > clap & relocate

--- src/main.rs
+++ src/main.rs
@@ -29,26 +29,28 @@ fn inside_docker() -> bool {
 }
 
 fn main() {
-    let matches = clap_app!(myapp =>
-        (version: "1.0")
-        (author: "Alex Yang <aleozlx@gmail.com>")
-        (about: "YAML driven Docker DevOps")
-        (@arg DOCKER_STEP: --docker-step "For Docker use only: run a specific step with docker")
-        (@arg RELOCATE: --relocate "Relocation of the playbook inside docker")
+    let matches = clap_app!(playbook =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg DOCKER_STEP: --("docker-step") "For Docker use ONLY: run a specific step with docker")
+        (@arg RELOCATE: --relocate "Relocation of the playbook inside docker, required when using abs. path")
         (@arg PLAYBOOK: +required "YAML playbook")
     ).get_matches();
-    setup_logger();
 
-    if inside_docker() {
-        let playbook = std::path::Path::new(matches.value_of("PLAYBOOK").unwrap());
-        if playbook.is_absolute() {
-            // Custom playbooks must be self-mounted with relocation specified at cmdline.
-            //             assert args.relocate is not None
-            //             PLAYBOOK = os.path.join(args.relocate, os.path.basename(PLAYBOOK))
-        }
-        else {
-            // System provided playbooks are automatically mounted read-only
-            //     PLAYBOOK = os.path.join('/opt/docker-playbook', PLAYBOOK)
-        }
+    if let Err(e) = setup_logger() {
+        panic!("ERROR {}", e);
     }
+
+    let playbook = std::path::Path::new(matches.value_of("PLAYBOOK").unwrap());
+    let playbook = if inside_docker() && playbook.is_absolute() {
+            // Absolute path to the playbook must be self-mounted with relocation specified at cmdline,
+            //   because we cannot read any content of the playbook without locating it first.
+            std::path::Path::new(matches.value_of("RELOCATE").expect("Missing a `--relocate` flag"))
+                .join(std::path::Path::new(matches.value_of("PLAYBOOK").unwrap()).file_name().unwrap())
+    }
+    else {
+        playbook.to_path_buf()
+    };
+
 }
