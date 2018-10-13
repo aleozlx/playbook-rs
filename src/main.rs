25bc56ea1fff72977a8f8011496f74735a69b311
Alex Yang <aleozlx@gmail.com>
2018-10-12 21:16:46 -0500

    + inside_docker()

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,15 @@ fn setup_logger() -> Result<(), fern::InitError> {
     Ok(())
 }
 
+fn inside_docker() -> bool {
+    let status = std::process::Command::new("grep").args(&["-q", "docker", "/proc/1/cgroup"])
+        .status().expect("I/O error");
+    match status.code() {
+        Some(code) => code==0,
+        None => unreachable!()
+    }
+}
+
 fn main() {
     let matches = clap_app!(myapp =>
         (version: "1.0")
@@ -30,4 +39,16 @@ fn main() {
     ).get_matches();
     setup_logger();
 
+    if inside_docker() {
+        let playbook = std::path::Path::new(matches.value_of("PLAYBOOK").unwrap());
+        if playbook.is_absolute() {
+            // Custom playbooks must be self-mounted with relocation specified at cmdline.
+            //             assert args.relocate is not None
+            //             PLAYBOOK = os.path.join(args.relocate, os.path.basename(PLAYBOOK))
+        }
+        else {
+            // System provided playbooks are automatically mounted read-only
+            //     PLAYBOOK = os.path.join('/opt/docker-playbook', PLAYBOOK)
+        }
+    }
 }
