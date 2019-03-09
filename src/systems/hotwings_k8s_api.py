c3d02f967e4cfe58b8abb1a7888ccc375c71befa
Alex Yang <aleozlx@gmail.com>
2019-03-08 19:28:42 -0600

    +some debug info to the k8s api script

--- src/systems/hotwings_k8s_api.py
+++ src/systems/hotwings_k8s_api.py
@@ -32,11 +32,14 @@ def join_job(job_spec):
     # possible pod phases: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase
     terminal_phases = set(['Succeeded', 'Failed', 'Unknown', 'Completed'])
     refresh = lambda: get_pods_status(get_pods(job_spec))
+    print('hotwings_k8s_api.py/join_job()', file=sys.stderr, flush=True)
     states = refresh()
+    print('init states', states, file=sys.stderr, flush=True)
     while not all((s in terminal_phases) for s in states.values()):
-        print(states, file=sys.stderr)
+        print(states, file=sys.stderr, flush=True)
         time.sleep(3)
         states = refresh()
+    print('final states', states, file=sys.stderr, flush=True)
 
 def k8s_provisioner(apicall, body):
     return globals()[apicall](body) # passing through any exceptions to playbook-rs
